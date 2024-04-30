814aed5e4e552bd62ddf308408c56bf9b686e2bc
Keyur-Rao <keyur@raoinformationtechnology.com>
2024-04-30 13:39:54 +0530

    change status of task done

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 
 const App = ()=> {
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  useEffect(() => {
-    // Check authentication status when the component mounts
-    checkAuthStatus();
-  }, []);
-
-  const checkAuthStatus = async () => {
-    try {
-      // get cookie value
-      const authToken = getCookie('authToken');
-      if (authToken) {
-        setLoggedIn(true);
-      }
-    } catch (error) {
-      console.error('Error checking authentication status:', error);
-    }
-  };
-
-  const getCookie = (cname)=> {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) === ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) === 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path='/' element={loggedIn ? <HomePage /> : <Navigate to={'/login'} replace={true} />} />
-        <Route exact path='/login' element={loggedIn ? <Navigate to={'/'} replace={true} /> : <LoginPage />} />
+        <Route exact path='/' element={ <HomePage />} />
+        <Route exact path='/login' element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,12 +8,52 @@ import { useNavigate } from "react-router-dom";
 const HomePage = () => {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [editTaskData, setEditTaskData] = useState({});
+  const [alertData, setAlertData] = useState({
+    title: "",
+    message: "",
+    type: "",
+  });
   const navigate = useNavigate();
-  
+
   useEffect(() => {
+    // Check authentication status when the component mounts
+    checkAuthStatus();
     // Fetch tasks when the component mounts
     fetchTasks();
   }, []);
+
+  const checkAuthStatus = async () => {
+    try {
+      // get cookie value
+      const authToken = getCookie("authToken");
+      if (authToken) {
+        setLoggedIn(true);
+        setShowAlert(true);
+        setAlertData({
+          title: "Success!",
+          message: "Login successfully.",
+          type: "success",
+        });
+        dismissAlert();
+      } else {
+        setLoggedIn(false);
+        navigate("/login");
+        return;
+      }
+    } catch (error) {
+      console.error("Error checking authentication status:", error);
+    }
+  };
+
+  const dismissAlert = () => {
+    setTimeout(() => {
+      setShowAlert(false);
+      setAlertData({ title: "", message: "", type: "" });
+    }, 3000);
+  };
 
   const getCookie = (cname) => {
     let name = cname + "=";
@@ -46,6 +86,12 @@ const HomePage = () => {
       setTasks(response.data.tasks);
     } catch (error) {
       console.error("Error fetching tasks:", error);
+      setShowAlert(true);
+      setAlertData({
+        title: "Token expired!",
+        message: "Please logout and login again.",
+        type: "danger",
+      });
     }
   };
 
@@ -69,9 +115,16 @@ const HomePage = () => {
       console.log("Res of add task ", response.data);
 
       if (response.data.success) {
+        setShowAlert(true);
+        setAlertData({
+          title: "Success!",
+          message: "Add task successfully.",
+          type: "success",
+        });
         // Refresh tasks list
         fetchTasks();
         setNewTask("");
+        dismissAlert();
       }
     } catch (error) {
       console.error("Error adding task:", error);
@@ -90,34 +143,107 @@ const HomePage = () => {
         },
       });
       if (response.data.success) {
+        setShowAlert(true);
+        setAlertData({
+          title: "Success!",
+          message: "Delete task successfully.",
+          type: "success",
+        });
         // Refresh tasks list
         fetchTasks();
+        dismissAlert();
       }
     } catch (error) {
       console.error("Error deleting task:", error);
     }
   };
 
-  const delete_cookie = (name)=> {
-    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  const editTask = async () => {
+    console.log("Edit task is ", editTaskData);
+    const authToken = getCookie("authToken");
+    console.log("Token is ", authToken);
+    try {
+      // Send request to backend to delete task
+      const response = await axios.put(
+        `${baseUrl}/api/tasks/${editTaskData._id}`,
+        {
+          description: editTaskData.description,
+          status: editTaskData.status,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      );
+      if (response.data.success) {
+        const editModal = document.getElementById("close-modal");
+        editModal.click();
+        setShowAlert(true);
+        setAlertData({
+          title: "Success!",
+          message: "Edit task successfully.",
+          type: "success",
+        });
+        // Refresh tasks list
+        fetchTasks();
+        dismissAlert();
+      }
+    } catch (error) {
+      console.error("Error deleting task:", error);
+    }
   };
 
-  const handleLogout = ()=> {
-    delete_cookie('authToken');
-    navigate('/login');
-  }
+  const delete_cookie = (name) => {
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+  };
+
+  const handleLogout = () => {
+    delete_cookie("authToken");
+    navigate("/login");
+  };
 
   return (
     <React.Fragment>
-    <nav className="navbar navbar-expand-lg navbar-primary bg-primary">
+      <nav className="navbar navbar-expand-lg navbar-primary bg-primary">
         <div className="container-fluid flex justify-content-lg-between">
-          <a className="navbar-brand text-light" href="#">Todo List</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <a className="navbar-brand text-light" href="#">
+            Todo List
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <button className="btn btn-light mb-2 mb-lg-0 text-primary" onClick={handleLogout}>{'logout'}</button>
+          <button
+            className="btn btn-light mb-2 mb-lg-0 text-primary"
+            onClick={handleLogout}
+          >
+            {"logout"}
+          </button>
         </div>
       </nav>
+      {showAlert && (
+        <div
+          className={`alert alert-${alertData.type} alert-dismissible fade show`}
+          role="alert"
+        >
+          <strong>{alertData.title}</strong> {alertData.message}
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="Close"
+          ></button>
+        </div>
+      )}
       <div className="container mt-5">
         <h1 className="text-center">Task Management</h1>
         <div>
@@ -134,16 +260,101 @@ const HomePage = () => {
               onChange={(e) => setNewTask(e.target.value)}
             ></textarea>
           </div>
-          <button onClick={addTask} className="btn btn-primary">Add Task</button>
+          <button onClick={addTask} className="btn btn-primary">
+            Add Task
+          </button>
         </div>
         <ul className="list-group mt-5">
           {tasks.map((task) => (
             <li key={task._id} className="list-group-item">
               {task.description}
-              <button onClick={() => deleteTask(task._id)} className="btn btn-danger float-end">Delete</button>
+              <h5 className="d-inline">
+                <span className="mx-3 badge text-bg-secondary">
+                  {" "}
+                  {task.status}
+                </span>
+              </h5>
+              <div className="float-end">
+                <button
+                  data-bs-toggle="modal"
+                  data-bs-target="#editModal"
+                  onClick={() => setEditTaskData(task)}
+                  className="btn btn-outline-primary mx-3"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => deleteTask(task._id)}
+                  className="btn btn-outline-danger"
+                >
+                  Delete
+                </button>
+              </div>
             </li>
           ))}
         </ul>
+      </div>
+
+      <div
+        className="modal fade"
+        id="editModal"
+        tabIndex="-1"
+        aria-labelledby="editModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="editModalLabel">
+                Edit task
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <textarea
+                className="form-control"
+                id="addtask"
+                rows="3"
+                placeholder="describe a small note on task"
+                value={editTaskData.description}
+                onChange={(e) =>
+                  setEditTaskData((prev) => ({
+                    ...prev,
+                    description: e.target.value,
+                  }))
+                }
+              ></textarea>
+              <select className="dropdown bg-secondary text-light px-3 py-1 rounded" value={editTaskData.status} onChange={(e)=> setEditTaskData((prev)=> ({ ...prev, 'status': e.target.value}))}>
+                  <option className="dropdown-item text-light" value='Pendding'> {"Pendding"} </option>
+                  <option className="dropdown-item text-light" value='In progress'> {"In progress"} </option>
+                  <option className="dropdown-item text-light" value='Testing'> {"Testing"} </option>
+                  <option className="dropdown-item text-light" value='Completed'> {"Completed"} </option>
+              </select>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                id="close-modal"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={editTask}
+              >
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
