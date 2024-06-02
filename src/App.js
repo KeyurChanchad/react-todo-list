6b0bba6830ab3f192037db7ac8020c24ab018124
KeyurChanchad <chanchadkeyur2001@gmail.com>
2024-06-02 11:54:36 +0530

    Use hash router instead of browser

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 
 const App = ()=> {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route exact path='/' element={ <HomePage />} />
         <Route exact path='/login' element={<LoginPage />} />
         <Route exact path='/signup' element={<SignupPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
